0e11a9d4aea67f0cf5ba1760e07b5b768c36c938
920993517@qq.com <920993517@qq.com>
2023-12-01 16:41:40 +0800

    dd

--- middleware/cors.go
+++ middleware/cors.go
@@ -13,7 +13,7 @@ func Cors() gin.HandlerFunc {
 			//接收客户端发送的origin （重要！）
 			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 
-			c.Header("Access-Control-Allow-Origin", "http://localhost:3000") // 可将将 * 替换为指定的域名
+			//c.Header("Access-Control-Allow-Origin", "http://localhost:3000") // 可将将 * 替换为指定的域名
 			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 			c.Header("Access-Control-Allow-Headers", "XMLHttpRequest, X_FILENAME, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with, Pragma")
 			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
